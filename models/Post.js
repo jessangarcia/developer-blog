225cf74842e0ff046a2c3d37964d7a73427a4937
jessan <jessenia.garcia998@gmail.com>
2021-11-09 11:36:25 -0700

    updated user routes

--- models/Post.js
+++ models/Post.js
@@ -15,6 +15,10 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
+        post_comment: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         user_id: {
             type: DataTypes.INTEGER,
             references: {
